8acf5feef638c63156186a7659cd24b49b047757
Ganesh Neupane <ganeshneupane1357@gmail.com>
2026-01-21 21:33:36 +0545

    fixed cors issue

--- KrishiKarobarBackend/backend/settings.py
+++ KrishiKarobarBackend/backend/settings.py
@@ -152,7 +152,7 @@ CORS_ALLOWED_ORIGINS = [
     'http://localhost:3000',
     'http://localhost:5173',
     'http://127.0.0.1:5173',
-    'https://krishikarobar-farmercustomer-marketplace.onrender.com/',
+    'https://krishikarobar-farmercustomer-marketplace.onrender.com',
 ]
 
 REST_FRAMEWORK = {
